4018513b4a40e48a3769c13132f59cdaa7603ed5
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-10-14 17:07:07 +0500

    Refactored error diplaying and added comments to code

--- site/form.php
+++ site/form.php
@@ -1,4 +1,5 @@
 <?php
+    // parse configuration file with data for db connection
     $config = parse_ini_file('/home/mathew/Documents/work/ktelekom_test_task/ktelekom-test/db.ini');
     // connect to a db
     $connection = mysqli_connect("localhost", $config['username'], $config['password'], $config['db']);
@@ -6,8 +7,12 @@
     if(!$connection) {
         echo 'Connection error: ' . mysqli_connect_error(); //only for developing process, remove before production
     }
+    // initialise assoc array for possible error messages
+    $errors = array('serial_number'=>'', 'hardware_type'=>'');
 
+    // check if submit button was pressed
     if(isset($_POST['submit'])) {
+        // initialising variables for data received from form
         $sn = '';
         $type = '';
 
@@ -15,36 +20,40 @@
         // regexp for dlink /^[0-9][0-9A-Z]{2}[A-Z]{2}[0-9A-Z][-_@][0-9A-Z][a-z]{2}$/
         // regexp for dlink s /^[0-9][0-9A-Z]{2}[A-Z]{2}[0-9A-Z][-_@][0-9A-Z]{3}$/
 
+        // check if type was not selected
         if($_POST['type'] == 'Не выбрано') {
-            echo 'You should select the hw type';
+            $errors['hardware_type'] = 'You should select the hardware type';
         } else {
             $type = mysqli_real_escape_string($connection, $_POST['type']);
         }
-
+        //make a query to a db to get the regexp of current hw type
         $query = "SELECT sn_mask FROM hw_type WHERE hw_type='$type'";
         $result = mysqli_query($connection, $query);
         $sn_mask = mysqli_fetch_all($result, MYSQLI_ASSOC)[0]['sn_mask'];
         mysqli_free_result($result);
-
+        //check if serial number is empty
         if (empty($_POST['serial_number'])) {
-            echo 'A serial number is required <br />';
+            $errors['serial_number'] = 'A serial number is required';
         } else {
             $sn = mysqli_real_escape_string($connection, $_POST['serial_number']);
-            if(!preg_match($sn_mask, $sn)) {
-                echo 'Serial number must match the current hardware type mask! <br />';
+            if(!preg_match($sn_mask, $sn)) {    // check for matching the regexp
+                $errors['serial_number'] = 'Serial number must match the current hardware type mask!';
             }
         }
-
+        // make a query to a db to get the id of current type
+        //TODO make this block as a function in order not to repeat myself
         $query = "SELECT id FROM hw_type WHERE hw_type='$type'";
         $result = mysqli_query($connection, $query);
         $type_id = mysqli_fetch_all($result, MYSQLI_ASSOC)[0]['id'];
         mysqli_free_result($result);
-
+        // generating a query to insert a new record to a db
         $query = "INSERT INTO hw_actual(type_id,serial_number) VALUES ('$type_id', '$sn')";
+
+        $status = '';   //setting the status variable in order to store status of the operation to echo it at the end of form
         if(mysqli_query($connection, $query)) {
-            echo 'Item was successfully added to a database.';
+            $status = 'Item was successfully added to a database.';
         } else {
-            echo 'Error adding values to the database: ' . mysqli_error($connection); // only for development process, remove before production
+            $status = 'Error adding values to the database: ' . mysqli_error($connection); // only for development process, remove before production
         }
         mysqli_close($connection);
     }
@@ -58,14 +67,17 @@
 </head>
 <body>
 <form action="form.php" method="post">
-    <p>Серийный номер: <input type="text" name="serial_number" /></p>
-    <p>Тип оборудования: <select name="type" id="types">
+    <label>Серийный номер: <input type="text" name="serial_number" /> </label>
+    <div class="red-text"><?php echo $errors['serial_number']; ?></div>
+    <label>Тип оборудования: <select name="type" id="types">
             <option selected="selected">Не выбрано</option>
         <option value="TP-Link TL-WR74">TP-Link TL-WR74</option>
         <option value="D-Link DIR-300">D-Link DIR-300</option>
         <option value="D-Link DIR-300 S">D-Link DIR-300 S</option>
-    </select> </p>
+    </select> </label>
+    <div class="red-text"><?php echo $errors['hardware_type']; ?></div>
     <p><input type="submit" name="submit" value="Добавить"/></p>
+    <div class="red-text"><?php echo $status; ?></div>
 </form>
 </body>
 </html>
