f3cffc6847550ce019a69039995b80b56a26a6a8
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-10-18 22:35:01 +0500

    Improved location of a style html tag

--- site/form.php
+++ site/form.php
@@ -77,13 +77,12 @@
     <meta charset="UTF-8">
     <title>Form</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style >
+        .red-text {
+            color:red;
+        }
+    </style>
 </head>
-<style >
-    .red-text {
-        color:red;
-    }
-</style>
-
 <body>
 <div class="d-flex justify-content-center align-self-center">
     <form action="form.php" method="post">
