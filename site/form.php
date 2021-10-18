5087ca602c4040aed7bcd3a5ac195ec30da6620f
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-10-18 23:55:27 +0500

    Beautified html

--- site/form.php
+++ site/form.php
@@ -144,11 +144,15 @@ if (isset($_POST['submit']))
             <div class="red-text"><?php echo $errors['hardware_type']; ?></div>
         </div>
 
-        <button type="submit" name= "submit" class="btn btn-primary">Add</button>
+        <button type="submit" name="submit" class="btn btn-primary">Add</button>
         <div><?php echo $status; ?></div>
     </form>
-<main>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <main>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+                integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+                crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+                integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+                crossorigin="anonymous"></script>
 </body>
 </html>
