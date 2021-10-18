f9a5348a931a8925e23746dbde32012649c53b12
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-10-18 22:32:58 +0500

    Improved CSS so now everything displays correctly

--- site/form.php
+++ site/form.php
@@ -58,7 +58,7 @@
                 $query = "INSERT INTO hw_actual(type_id,serial_number) VALUES ('$type_id', '$sn')";
                 if(mysqli_query($connection, $query)) { //sending a query
                     $status = 'Item was successfully added to a database.';
-                  header('Location: form.php');
+//                  header('Location: form.php');
                 } else {
                     $status = 'Error adding values to the database: ' . mysqli_error($connection); // only for development process, remove before production
                 }
@@ -78,22 +78,37 @@
     <title>Form</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body>
-    <form action="form.php" method="post">
-        <label>Серийный номер: <input type="text" name="serial_number" value="<?php echo htmlspecialchars($_POST['serial_number']); ?>" /> </label>
-        <div class="red-text"><?php echo $errors['serial_number']; ?></div>
-        <label>Тип оборудования:
-            <select name="type" id="types">
-                <?php foreach ($hw_types as $value) {?>
-                <option value="<?php echo $value ?>" <?= (isset($_POST['type']) && $_POST['type'] == $value) ? 'selected' : '' ?>><?php echo $value ?></option>>
-                <?php } ?>
-            </select>
-        </label>
+<style >
+    .red-text {
+        color:red;
+    }
+</style>
 
-        <div class="red-text"><?php echo $errors['hardware_type']; ?></div>
-        <p><input type="submit" name="submit" value="Добавить"/></p>
+<body>
+<div class="d-flex justify-content-center align-self-center">
+    <form action="form.php" method="post">
+        <div class="mb-3">
+            <label class="form-label">Серийный номер:
+                <input type="text" name="serial_number" class="form-control"
+                       value="<?php echo htmlspecialchars($_POST['serial_number']); ?>"/>
+            </label>
+            <div class="red-text"><?php echo $errors['serial_number']; ?></div>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Тип оборудования:
+                <select name="type" class="form-select">
+                    <?php foreach ($hw_types as $value) { ?>
+                        <option value="<?php echo $value ?>" <?= (isset($_POST['type']) && $_POST['type'] == $value) ? 'selected' : '' ?>><?php echo $value ?></option>>
+                    <?php } ?>
+                </select>
+            </label>
+            <div class="red-text"><?php echo $errors['hardware_type']; ?></div>
+        </div>
+
+        <button type="submit" name= "submit" class="btn btn-primary">Add</button>
         <div class="red-text"><?php echo $status; ?></div>
     </form>
+</div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
