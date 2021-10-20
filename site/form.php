c0b2e87581d252f44eb7aca76a16e7f2028c23ab
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-10-20 22:36:51 +0500

    Moved css to a separate file

--- site/form.php
+++ site/form.php
@@ -94,34 +94,7 @@ if (isset($_POST['submit'])) {
     <title>Form</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <style>
-        .red-text {
-            color: red;
-        }
-
-        .green-text {
-            color: green;
-        }
-
-        .form-signin {
-            width: 100%;
-            max-width: 330px;
-            padding: 15px;
-            margin: auto;
-        }
-
-        html, body {
-            height: 100%;
-        }
-
-        body {
-            display: flex;
-            align-items: center;
-            padding-top: 40px;
-            padding-bottom: 40px;
-            background-color: #f5f5f5;
-        }
-    </style>
+    <link href="style.css" rel="stylesheet">
 </head>
 <body>
 <main class="form-signin">
